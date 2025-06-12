30068170a11bef38ace67dd0ab68a6e70baf1fd4
Pony-min <hmkim23@weaversmind.com>
2025-06-12 15:25:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ def index():
 
 @app.route('/romanize', methods=['POST'])
 def convert():
-    data = request.get_json()
-    text = data.get("text", "")
-    result = romanize(text)
-    return jsonify({"result": result})
+    try:
+        data = request.get_json()
+        text = data.get("text", "")
+        result = romanize(text)
+        return jsonify({ "result": result })
+    except Exception as e:
+        return jsonify({ "error": str(e) }), 500  # 오류 반환
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
