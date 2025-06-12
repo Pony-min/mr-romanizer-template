0c1664b34893ce14f8c326be49c8275fd472ef79
Pony-min <hmkim23@weaversmind.com>
2025-06-12 13:03:23 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from mcr_romanization import romanize
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "MR Romanizer API is running!"
+
+@app.route('/romanize', methods=['POST'])
+def api_romanize():
+    data = request.get_json()
+    text = data.get('text', '')
+    result = romanize(text)
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
