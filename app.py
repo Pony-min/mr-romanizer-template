315ff96ac2f18bb4311547ded422b0a7e407d3e3
Pony-min <hmkim23@weaversmind.com>
2025-06-12 13:19:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, request, jsonify
+from mcr_romanization import romanize
 
 app = Flask(__name__)
 
-def romanize(text):
-    return "mr-" + text
-
 @app.route('/')
 def index():
-    return "MR Romanizer API is running!"
+    return "McCune–Reischauer API is running!"
 
 @app.route('/romanize', methods=['POST'])
 def convert():
     data = request.get_json()
     text = data.get("text", "")
-    return jsonify({"result": romanize(text)})
+    result = romanize(text)
+    return jsonify({"result": result})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
